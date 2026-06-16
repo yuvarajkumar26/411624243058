452c724313b8cd6d0a517f7ae87ab44e91c98f81
Yuvarjkumar <yuvarajkumar092006@gmail.com>
2026-06-16 11:15:31 +0530

    Event Driven Arch

---
+++ event_driven.js
@@ -0,0 +1,38 @@
+console.log("A");
+console.log("B");
+console.log("C");
+
+// const promise=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log("Hi")
+//     },2000);
+//     setTimeout(()=>{
+//         console.log("Done");
+//     },0);
+//     console.log("D");
+//     const success=true;
+//     if(success){
+//         console.log("Done")
+//     }
+//     else{
+//         console.log("No");
+//     }
+//  });
+// promise.then(res=>{console.log(res)}).catch(err=>{console.log(err)})
+
+console.log(1);
+console.log(2);
+console.log("Hi");
+
+function example(){
+    console.log("Hello");
+}
+
+const eg=()=>{
+    const a=1;
+    const b=2;
+    console.log(a+b);
+}
+
+example();
+eg();
